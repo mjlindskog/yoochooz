f3b3d59b4c0b393daf8ee8db28650169dbbf5509
mjlindskog <lindskogmarkj@gmail.com>
2024-11-25 00:23:02 -0500

    update file structure

---
+++ frontend/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>yoochooz</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
